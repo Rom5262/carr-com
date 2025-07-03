6d9a4bf22267d2c703b96ca6d10a1c3d07a6f64b
Rom5262 <garay2772@gmail.com>
2025-07-03 16:53:51 -0600

    Grafica

--- main.py
+++ main.py
@@ -52,3 +52,63 @@ ax.set_title('Distribución de órdenes por hora del día')
 ax.set_xlabel('Hora del Día')
 ax.set_ylabel('Frecuencia')
 st.pyplot(fig)
+
+
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Cargar los datos
+orders = pd.read_csv('instacart_orders.csv', sep=';')
+
+# Agrupar por hora y contar usuarios
+usuarios_por_dia = orders.groupby('order_dow')['user_id'].count()
+
+# Crear gráfico
+fig, ax = plt.subplots(figsize=(8, 4))
+usuarios_por_dia.plot(
+    ax=ax,
+    marker='o',
+    linestyle='-',
+    color='b',
+    grid=True
+)
+
+# Personalización
+ax.set_title('Usuarios que hacen compras por dia')
+ax.set_xlabel('Día de la Semana')
+ax.set_ylabel('Usuarios por día')
+
+# Mostrar en Streamlit
+st.pyplot(fig)
+
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import calendar
+
+# Cargar datos
+orders = pd.read_csv('instacart_orders.csv', sep=';')
+
+# Crear gráfico
+fig, ax = plt.subplots()
+
+# Histograma con 7 bins (uno por cada día)
+sns.histplot(
+    data=orders,
+    x='order_dow',
+    bins=7,  # Uno por cada día (0-6)
+    ax=ax,
+    color='orange'
+)
+
+# Personalización
+ax.set_title('Compras por día de la Semana')
+ax.set_xlabel('Día de la Semana')
+ax.set_ylabel('Usuarios por Día')
+ax.set_xticks(range(7))  # Posiciones de las etiquetas
+ax.set_xticklabels(calendar.day_name[:7])  # Etiquetas con nombres de los días
+
+# Mostrar gráfico en Streamlit
+st.pyplot(fig)
